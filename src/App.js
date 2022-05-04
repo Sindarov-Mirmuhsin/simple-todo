69710be18d09e26adb4997d3927693c2d08afe17
Sindarov-Mirmuhsin <therealgreen416@gmail.com>
2022-05-04 22:47:01 +0500

    Initail commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,67 @@
-import React, { useState } from "react";
-import TodoItem from "./Components/TodoItem/TodoItem";
-import "./Components/Assets/main.css";
-
-
+import { useState } from 'react';
+import TodoItem from './Components/TodoItem/TodoItem';
+import './Components/Assets/main.css';
 
 function App() {
+	const [todos, setTodos] = useState(
+		JSON.parse(window.localStorage.getItem('todos')) || [],
+	);
 
-  const [todos, setTodos] = useState(JSON.parse(window.localStorage.getItem("todos")) || []);
+	const [status, setStatus] = useState();
 
-  console.log(todos);
+	console.log(todos);
 
+	function handleCreateTodo(evt) {
+		const newTodo = {
+			id: todos[todos.length - 1]?.id + 1 || 0,
+			title: evt.target.value,
+			isCompleted: false,
+		};
+		if (evt.code === 'Enter') {
+			evt.target.value = null;
+			window.localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
+			return setTodos([...todos, newTodo]);
+		}
+	}
 
-  function handleCreateTodo(evt) {
-    const newTodo = {
-      id: todos[todos.length - 1]?.id + 1 || 0,
-      title: evt.target.value,
-      isCompleted: false
-    }
-    if (evt.code === "Enter") {
-      evt.target.value = null;
-      window.localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
-      return setTodos([...todos, newTodo])
-    }
-  }
+	function handleDeleteTodo(evt) {
+		const deletedTodoId = evt.target.dataset.todoId - 0;
+		const filterArray = todos.filter((item) => item.id !== deletedTodoId);
+		window.localStorage.setItem('todos', JSON.stringify(filterArray));
+		return setTodos(filterArray);
+	}
 
-  
+	function handleCompleted(evt) {
+		const completedId = evt.target.dataset.todoId - 0;
+		const findedItem = todos.find((item) => item.id === completedId);
+		findedItem.isCompleted = !findedItem.isCompleted;
+		return setTodos([...todos]);
+	}
 
-  function handleDeleteTodo(evt) {
-    const deletedTodoId = evt.target.dataset.todoId - 0
-    const filterArray = todos.filter(item => item.id !== deletedTodoId)
-    window.localStorage.setItem("todos", JSON.stringify(filterArray));
-    return setTodos(filterArray)
-  }
+	function getAll() {
+		setStatus('all');
+		setTodos(JSON.parse(window.localStorage.getItem('todos')));
+	}
 
-  function handleCompleted(evt) {
-    const completedId = evt.target.dataset.todoId - 0
-    const findedItem = todos.find(item => item.id === completedId)
-    findedItem.isCompleted = !findedItem.isCompleted
-    return setTodos([...todos]);
+	function getActive() {
+		setStatus(false);
+		setTodos(
+			JSON.parse(window.localStorage.getItem('todos')).filter(
+				(item) => item.isCompleted === false,
+			),
+		);
+	}
 
-  }
+	function getCompleted() {
+		setStatus(true);
+		setTodos(
+			JSON.parse(window.localStorage.getItem('todos')).filter(
+				(item) => item.isCompleted === true,
+			),
+		);
+	}
 
-  return (
+	return (
 		<div>
 			<input
 				className='input'
@@ -50,7 +70,7 @@ function App() {
 				placeholder='todo...'
 			/>
 			<ul className='list'>
-				{todos?.map((item) => (
+				{todos.map((item) => (
 					<TodoItem
 						key={item.id}
 						handleCompleted={handleCompleted}
@@ -62,6 +82,17 @@ function App() {
 					</TodoItem>
 				))}
 			</ul>
+			<div className='box'>
+				<button className='box__btn box__btn--all' onClick={getAll}>
+					All
+				</button>
+				<button className='box__btn box__btn--active' onClick={getActive}>
+					Active
+				</button>
+				<button className='box__btn box__btn--completed' onClick={getCompleted}>
+					Completed
+				</button>
+			</div>
 		</div>
 	);
 }
